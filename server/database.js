38c3d973cfb1463e2374eba1a052ba311d762c65
kwakster4 <kimbyung4@utexas.edu>
2021-05-08 16:13:28 -0500

    fixed bug with get questions not returning questions with empty answers

--- server/database.js
+++ server/database.js
@@ -3,7 +3,8 @@ const schemas = require('./../schemas/serverSchemas');
 const moment = require('moment');
 // index behavior recommended to be turned off for production, as index creation can have performance impact. turn off with autoIndex false.
 // e.g. mongoose.connect('mongodb://user:pass@localhost:port/database', { autoIndex: false });
-mongoose.connect('mongodb://localhost/sdc_q_a', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true});
+mongoose.connect('mongodb://localhost/sdc_q_a', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
+// mongoose.set('maxTimeMS', 1);
 const Question = mongoose.model('Question', schemas.newQuestionSchema,
 'questions');
 const MaxId = mongoose.model('MaxId', schemas.maxIdSchema, 'maxids');
@@ -16,11 +17,15 @@ const getQs = function(product_id, page, count) {
   // return Question.find({'product_id': product_id, reported:false}, {limit: count});
   // when using aggregate, reported returns as 0 instead of boolean.
 
-  // return Question.aggregate([{$match: {$and: [{reported: 0}, {'product_id': product_id}]}}, {$limit: count}])
-  return Question.aggregate([{$match: {$and: [{reported: {$ne:1 }}, {'product_id': product_id}]}}, {$unwind: '$answers'}, {$match: {'answers.reported': {$ne: 1}}}, {$group: {_id:'$id', answers: {$push: '$answers'}, question_body: {$first: '$body'}, 'question_date': {$first: '$date_written'}, 'asker_name':{$first:'$asker_name'}, 'question_helpfulness':{$first:'$helpful'}}}, {$limit: count}])
+  // the below comment works only if the question has an answer
+  // return Question.aggregate([{$match: {$and: [{reported: {$ne:1 }}, {'product_id': product_id}]}}, {$unwind: '$answers'}, {$match: {'answers.reported': {$ne: 1}}}, {$group: {_id:'$id', answers: {$push: '$answers'}, question_body: {$first: '$body'}, 'question_date': {$first: '$date_written'}, 'asker_name':{$first:'$asker_name'}, 'question_helpfulness':{$first:'$helpful'}}}, {$limit: count}]).option({maxTimeMS: 50, allowDiskUse: true})
+  return Question.aggregate([{$match: {$and: [{reported: {$ne:1 }}, {'product_id': product_id}]}}, {$limit: count}])
     .then((questions)=>{
       questions = questions.map((question)=> {
-        let answers = question.answers.map((answer)=>{
+        let answers = question.answers.filter((answer)=>{
+          return answer.reported !== 1;
+        })
+        answers = answers.map((answer)=>{
           let photos = answer.photos.map((photo)=>{
             return {'id': photo.id, 'url': photo.url}
           });
@@ -35,22 +40,23 @@ const getQs = function(product_id, page, count) {
         });
         let answersObj = {};
         for (let answer of answers) {
+          console.log(answer);
           answersObj[answer.id] = answer;
         }
-        question.question_id = question._id;
-        delete question._id;
-        question.reported= false;
-        question.answers = answersObj;
-        return question;
-        // return {
-        //   question_id: question.id,
-        //   question_body: question.body,
-        //   question_date: question.date_written,
-        //   asker_name: question.asker_name,
-        //   question_helpfulness: question.helpful,
-        //   reported: false,
-        //   answers: answersObj
-        // }
+        // question.question_id = question._id;
+        // delete question._id;
+        // question.reported= false;
+        // question.answers = answersObj;
+        // return question;
+        return {
+          question_id: question.id,
+          question_body: question.body,
+          question_date: question.date_written,
+          asker_name: question.asker_name,
+          question_helpfulness: question.helpful,
+          reported: false,
+          answers: answersObj
+        }
       })
       return questions;
     });
@@ -59,7 +65,7 @@ const getQs = function(product_id, page, count) {
 const setQ = function(newQ) {
   let currentTime = moment().format('YYYY-MM-DD');
   newQ.date_written = currentTime;
-  return MaxId.findOneAndUpdate({for: 'questions'}, {$inc: {'maxId': 1}})
+  return MaxId.findOneAndUpdate({for: 'questions'}, {$inc: {'maxId': 1}}).maxTimeMS(50)
     .then((id)=>{
       newQ.id = id.maxId + 1;
       return Question.create(newQ);
@@ -73,7 +79,7 @@ const setQ = function(newQ) {
 
 const getAs = function(question_id, page, count) {
   // get all non-reported Answers for that product
-  return Question.aggregate([{$match: {id: question_id}}, {$unwind: '$answers'}, {$match: {'answers.reported': {$ne: 1}}}, {$project: {'answers':1, _id:0}}, {$limit: count}])
+  return Question.aggregate([{$match: {id: question_id}}, {$unwind: '$answers'}, {$match: {'answers.reported': {$ne: 1}}}, {$project: {'answers':1, _id:0}}, {$limit: count}]).option({maxTimeMS: 50, allowDiskUse: true})
     .then((answers)=>{
       return answers.map((answer)=>{
         answer = answer.answers;
@@ -95,7 +101,7 @@ const getAs = function(question_id, page, count) {
 const setA = function(question_id, newA) {
   let currentTime = moment().format('YYYY-MM-DD');
   newA.date_written = currentTime;
-  return MaxId.findOneAndUpdate({for: 'answers'}, {$inc: {'maxId': 1}})
+  return MaxId.findOneAndUpdate({for: 'answers'}, {$inc: {'maxId': 1}}).maxTimeMS(50)
     .then((id)=>{
       newA.id = id.maxId + 1;
       return Question.updateOne({id: question_id}, {$push: {answers: newA}})
@@ -104,16 +110,16 @@ const setA = function(question_id, newA) {
 
 const helpQ = function(question_id) {
   // access and change helpfulness of question.
-  return Question.updateOne({id: question_id}, {$inc:{'helpful': 1}});
+  return Question.updateOne({id: question_id}, {$inc:{'helpful': 1}}).maxTimeMS(50);
 };
 
 const reportQ = function(question_id) {
-  return Question.updateOne({id: question_id}, {$set:{'reported': 1}});
+  return Question.updateOne({id: question_id}, {$set:{'reported': 1}}).maxTimeMS(50);
 };
 
 const helpA = function(answer_id) {
   // may also get away with Question.update({answer.id: answer_id}, {$inc:{'answers.$.helpfulness':1}}), bc listed as index on mongo database
-  return Question.updateOne({'answers.id': answer_id}, {$inc:{'answers.$.helpful': 1}});
+  return Question.updateOne({'answers.id': answer_id}, {$inc:{'answers.$.helpful': 1}}).maxTimeMS(50);
   // Answer.findOne({id: answer_id}).select('question_id')
   //   .then((id)=>{
   //     return id.question_id;
@@ -127,7 +133,7 @@ const helpA = function(answer_id) {
 };
 
 const reportA = function(answer_id) {
-  return Question.updateOne({'answers.id': answer_id}, {$set:{'answers.$.reported': 1}});
+  return Question.updateOne({'answers.id': answer_id}, {$set:{'answers.$.reported': 1}}).maxTimeMS(50);
   // Answer.findOne({id: answer_id}).select('question_id')
   //   .then((id)=>{
   //     return id.question_id;
